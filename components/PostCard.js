2c645a5f7d44700f7796837b6843e04885c300b6
shun0918 <kkabosu386@gmail.com>
2021-03-25 23:15:12 +0900

    topページのレイアウト調整

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,77 +1,22 @@
 import Link from 'next/link'
+import styles from '../styles/components/PostCard.module.scss'
 
 function PostCard({ title, thumbnail, body, publishedAt ,updatedAt, slug }) {
   return (
-    <div className="post-card">
-      <div className="post-card__eyecatch--flame">
-        <Link
-          href={{ pathname: '/post', query: { slug:slug } }}
-          passHref={true}
-        >
-          <a className="post-card__eyecatch--link">
-            <img className="img" alt={thumbnail.fields.file.fileName} src={thumbnail.fields.file.url} width="280" height="160"/>
-          </a>
-        </Link>
-      </div>
-      <h2 className="post-card__title">
-        <Link
-          href={{ pathname: '/post', query: { slug:slug } }}
-          passHref={true}
-        >
-        {title}
-        </Link>
-      </h2>
-      <p className="datetime">投稿日:<time>{publishedAt}</time></p>
-      <style jsx>{`
-        .post-card {
-          position: relative;
-          padding: 0.75rem;
-          border-radius: 30px;
-          background: #F4E2CA;
-          box-shadow:  9px 9px 30px #c8b9a6,
-                      -9px -9px 30px #ffffee;
-          overflow: hidden;
-        }
-        .post-card__eyecatch--flame {
-        }
-        .post-card__eyecatch--link {
-          border-radius: 20px;
-          display: block;
-          overflow: hidden;
-          position: relative;
-        }
-        .post-card__eyecatch--link:before {
-          content: '';
-          display: block;
-          padding-top: 50%;
-        }
-        .post-card__eyecatch--link img {
-          width: 100%;
-          height: auto;
-          object-fit: cover;
-          object-position: center;
-          position: absolute;
-          top: 0;
-          left: 0;
-          right: 0;
-          left: 0;
-        }
-        .post-card__title {
-          color: #232323;
-          font-size: 1rem;
-          margin-top: 1rem
-        }
-        .text {
-          margin-top: -160px;
-          padding: 16px;
-          position: absolute;
-        }
-        .datetime {
-          font-size: 0.75rem;
-          margin: 0.5rem auto 1rem;
-        }
-      `}</style>
-    </div>
+    <Link
+      href={{ pathname: '/post', query: { slug:slug } }}
+      passHref={true}
+    >
+      <a className={styles["post-card--link"]}>
+        <div className={styles["post-card"]}>
+          <div className={styles["post-card__eyecatch"]}>
+            <img className={styles["post-card__eyecatch--img"]} alt={thumbnail.fields.file.fileName} src={thumbnail.fields.file.url} width="280" height="160"/>
+          </div>
+          <h2 className={styles["post-card__title"]}>{title}</h2>
+          <p className={styles["datetime"]}>投稿日:<time>{publishedAt}</time></p>
+        </div>
+      </a>
+    </Link>
   )
 }
 
