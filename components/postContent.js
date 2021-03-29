8c82edf4cad1879c66f2640d47b500af193b31ae
shun0918 <kkabosu386@gmail.com>
2021-03-29 23:45:48 +0900

    h2以降を使用するよう変更

--- components/postContent.js
+++ components/postContent.js
@@ -8,7 +8,7 @@ function PostContent({ title, thumbnail, body, publishedAt, updatedAt, slug }) {
     <article className={styles.content}>
       <div className={styles.content__head}>
         <img className={styles.content__eyecatch}　alt={thumbnail.fields.file.fileName} src={thumbnail.fields.file.url} width="640" height="360"/>
-        <h1 className={styles.content__title}>{ title }</h1>
+        <h2 className={styles.content__title}>{ title }</h2>
         <p className={styles.content__date}>投稿日:{" "}<date>{publishedAt}</date></p>
       </div>
       <div className={styles.content__body}>
