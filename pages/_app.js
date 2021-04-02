947802243e38c676efacf9c9d445017b0b22c1c3
shun0918 <kkabosu386@gmail.com>
2021-04-02 18:41:16 +0900

    webフォント適用

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/_app/destyle.css'
 import '../styles/_app/globals.scss'
+import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Damion&display=swap" rel="stylesheet" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
