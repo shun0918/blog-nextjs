12cf98f86dd9e273dd9e2c138e51712c51963e86
shun0918 <kkabosu386@gmail.com>
2021-04-03 12:31:28 +0900

    headerの構造を修正

--- components/Header.js
+++ components/Header.js
@@ -7,11 +7,13 @@ function Header() {
     <header className={styles.header}>     
       <div className={styles["grobal-nav"]}>
         <div className={styles["grobal-nav__inner"]}>
-          <Link href="/">
-            <a className={styles["grobalnav__logo--link"]}>
-              <h1 className={styles["grobal-nav__logo"]}>Shun Bibo Roku</h1>
-            </a>
-          </Link>
+          <h1 className={styles["grobal-nav__logo"]}>
+            <Link href="/">
+              <a className={styles["grobalnav__logo--link"]}>
+                <span>Shun Bibo Roku</span>
+              </a>
+            </Link>
+          </h1>
         </div>
       </div>
     </header>
