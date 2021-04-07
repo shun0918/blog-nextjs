820796fa7a1327c49c38c3eeb8f0fcd3f37014d7
shun0918 <kkabosu386@gmail.com>
2021-04-07 22:21:48 +0900

    description以外のOGP対応中

--- components/Ogp.js
+++ components/Ogp.js
@@ -1,26 +1,27 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import Head from 'next/head'
 function Ogp({ title, description, imagePath, type, path }) {
+  const location = process.env.NEXT_PUBLIC_LOCATION || 'https://shunbiboroku.com'
   const router = useRouter()
-  const url = "http://localhost" + path
+  const url = location + path
   const siteName = "Shun Bibo Roku"
 
 
   return (
-    <>
+    <Head>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      {/* <meta name="keywords" content={keyword} /> */}
       <meta property="og:type" content={type} />
       <meta property="og:url" content={url} />
       <meta property="og:image" content={imagePath} />
       <meta property="og:site_name" content={siteName} />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@tcr_jp" />
-      <meta name="twitter:url" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@kabosu_en" />
+      <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-    </>
+      <meta name="twitter:image" content={imagePath} />
+    </Head>
   )
 }
 export default Ogp;
