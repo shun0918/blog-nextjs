342cf7a9ff57d1dd23c0132e8486e366d04a13e3
shun0918 <kkabosu386@gmail.com>
2021-04-11 15:08:36 +0900

    タイトルタグをOGPコンポーネントで管理

--- components/Ogp.js
+++ components/Ogp.js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-function Ogp({ title, description, imagePath, type, path }) {
+function Ogp({ title, description, image, type, path }) {
   const location = process.env.NEXT_PUBLIC_LOCATION || 'https://shunbiboroku.com'
   const router = useRouter()
   const url = location + path
@@ -9,18 +9,19 @@ function Ogp({ title, description, imagePath, type, path }) {
 
   return (
     <Head>
+      <title>{title}</title>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:type" content={type} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={imagePath} />
+      <meta property="og:image" content={image} />
       <meta property="og:site_name" content={siteName} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@kabosu_en" />
       <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imagePath} />
+      <meta name="twitter:image" content={image} />
     </Head>
   )
 }
