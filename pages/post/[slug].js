c7ec1e925a2e4f221aa98bb635c9fccbf2c57c68
shun0918 <kkabosu386@gmail.com>
2021-04-12 20:47:31 +0900

    ssr用pageの用意

---
+++ pages/post/[slug].js
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { fetchPostBySlug } from '../lib/contentful/contentful'
+import PostContent from '../components/PostContent'
+import Ogp from '../components/Ogp'
+import styles from '../styles/pages/post.module.scss'
+import { _documentToReactComponents } from '../lib/contentful/_documentToReactComponents'
+
+
+export default function Post() {
+  const router = useRouter()
+  const [post, setPost] = useState({})
+  const [slug, setSlug] = useState(router.query.slug)
+  const [description, setDescription] = useState("")
+  const [image, setImage] = useState("")
+  useEffect(() => {
+    async function getPost() {
+      const postData = await fetchPostBySlug(slug)
+      console.log(postData);
+      setPost(postData)
+      setImage("https:" + postData.fields.thumbnail.fields.file.url)
+      if(post.fields && post.fields.body){
+        setDescription("現在Contentfulのリッチテキストからdescriptionを生成する方法を検討中です！")
+        // setDescription(_documentToReactComponents(post.fields.body))
+      }
+    }
+    getPost()
+  }, [router, slug])
+
+  return (
+    <>
+      {"fields" in post
+        ? <Ogp 
+          title={post.fields.title + "| Shun Bibo Roku"}
+          description={description}
+          image={image}
+          type="article"
+          path={"/post"+"?slug="+post.fields.slug}
+        />
+      : null}
+      <main className={styles.main}>
+        {"fields" in post
+          ? <PostContent 
+              title={post.fields.title}
+              thumbnail={post.fields.thumbnail}
+              body={post.fields.body}
+              publishedAt={post.fields.publishedAt}
+              updatedAt={post.fields.updatedAt}
+              slug={post.fields.slug}
+            />
+          : null }
+      </main>
+    </>
+  )
+}
