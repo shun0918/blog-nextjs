af6444a4c712c5da01888870ccf0faf20d9f08e1
shun0918 <kkabosu386@gmail.com>
2021-04-25 17:58:22 +0900

    worksのレイアウト作成

--- components/Abstruct.js
+++ components/Abstruct.js
@@ -1,12 +1,16 @@
 import styles from '../styles/components/abstruct.module.scss'
 
-export default Abstruct = ({ src, title, tag, description}) => {
+function Abstruct({ src, url, tags, title, description, width="1280", height="720"}) {
   return (
     <div className={styles.abstruct}>
-      <img src={ src } width={ width } height={ height } />
-      <h3>{ title }</h3>
-      <p>{tag.join("/")}</p>
-      <p>{ description }</p>
+      <a href={url} className={styles.abstruct__link}>
+        <img src={ src } width={ width } height={ height } className={styles.abstruct__image}/>
+        <h3 className={ styles.abstruct__title }>{ title }</h3>
+        <p className={ styles.abstruct__tags }>{ tags.join("/") }</p>
+        <p className={ styles.abstruct__description }>{ description }</p>
+      </a>
     </div>
   )
 }
+
+export default Abstruct
--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,53 @@
+import Abstruct from '../components/Abstruct'
+import SectionHeader from '../components/SectionHeader'
+import styles from '../styles/pages/works.module.scss'
+
 export default function works() {
+  const works = [
+    {
+      src: "/img/works/cannatech.png",
+      url: "#",
+      name: "株式会社cannatech(キャナテック)様",
+      tags: ["Planning","Design","WordPress","HTML","CSS(SCSS)"],
+      description: "CBD製品の商品企画、製造、販売を行っている株式会社CannaTech様のコーポレートサイトを作成させていただきました。プランニングからデザイン、コーディング、WordPressを一貫して担当させていただきました。"
+    },
+    {
+      src: "/img/works/cannatech.png",
+      url: "#",
+      name: "株式会社cannatech(キャナテック)様",
+      tags: ["Planning","Design","WordPress","HTML","CSS(SCSS)"],
+      description: "CBD製品の商品企画、製造、販売を行っている株式会社CannaTech様のコーポレートサイトを作成させていただきました。プランニングからデザイン、コーディング、WordPressを一貫して担当させていただきました。"
+    },
+    {
+      src: "/img/works/cannatech.png",
+      url: "#",
+      name: "株式会社cannatech(キャナテック)様",
+      tags: ["Planning","Design","WordPress","HTML","CSS(SCSS)"],
+      description: "CBD製品の商品企画、製造、販売を行っている株式会社CannaTech様のコーポレートサイトを作成させていただきました。プランニングからデザイン、コーディング、WordPressを一貫して担当させていただきました。"
+    },
+  ]
   return (
     <>
-      <h2>ya</h2>
+      <SectionHeader
+        title="Works"
+      />
+      <ul className={styles.works}>
+      { works.map((m) => (
+        <li className={styles.works__item}>
+          <Abstruct
+            src={m.src}
+            url={m.url}
+            title={m.name}
+            tags={m.tags}
+            description={m.description}
+            width="1280"
+            height="720"
+          />
+        </li>
+      ))
+
+      }
+      </ul>
     </>
   )
 }
