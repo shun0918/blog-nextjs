a6be87b57d38fb6451bf79544e528517e5bc88c6
shun0918 <kkabosu386@gmail.com>
2021-04-29 22:22:02 +0900

    React.FCに型を変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/components/Footer.module.scss';
-const Footer: JSX.Element = (
+const Footer: React.FC = () => (
   <footer className={styles.footer}>
     <div className={styles['content-info']}>
       <span>©️ 2021 created by Shun Nihei</span>
