4be9397e83a2c51a446a7933046c92631a8a83fd
shun0918 <kkabosu386@gmail.com>
2021-05-08 17:30:26 +0900

    名前変更

--- src/hooks/usePageView.ts
+++ src/hooks/usePageView.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import * as gtag from '~/lib/ga/gtag';
+import * as gtag from '~/lib/ga/ga';
 
 export default function usePageView(): void {
   const router = useRouter();
