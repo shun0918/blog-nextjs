1d2d1a42e74511015677be1f455bb6827dd6a123
shun <50100621+shun0918@users.noreply.github.com>
2022-04-14 06:46:05 +0900

    Copyright表記更新

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from '~/styles/components/Footer.module.scss';
 const Footer: React.FC = () => (
   <footer className={styles.footer}>
     <div className={styles['content-info']}>
-      <span>©️ 2021 created by Shun Nihei</span>
+      <span>©️ 2022 created by Shun Nihei</span>
     </div>
   </footer>
 );
