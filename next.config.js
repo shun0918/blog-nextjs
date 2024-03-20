b898b1f969731e37703bd9835e51fa1a2fffc11f
shun0918 <nihei@tebiki.co.jp>
2024-03-20 20:41:02 +0900

    chore: update packages

--- next.config.js
+++ next.config.js
@@ -1,22 +1,10 @@
 const path = require('path');
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
 
 module.exports = withPWA({
-  experimental: {
-    optimizeFonts: true,
-  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '~': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
-  pwa: {
-    dest: 'public',
-    // runtimeCaching: []
   },
 });
